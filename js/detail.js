b2ddf3a34a8193aca1999451cf15262ac3682f56
HugoAhoy <zhjd3302@outlook.com>
2019-12-23 00:59:55 +0800

    now you can add support and comment

--- js/detail.js
+++ js/detail.js
@@ -25,7 +25,7 @@ $(function(){
     }
 })
 
-getComment=function(supportId){
+getAllComment=function(supportId){
     var thistoken = localStorage.getItem("token");
     
     $.ajax({
@@ -38,12 +38,50 @@ getComment=function(supportId){
         success:function(data, status){ 
             console.log("data= ",data, "status=", status);
             localStorage.setItem("token", thistoken);
-            renderComment(data);
-
+            renderAllComment(data);
         }
     }); 
 
 }
+timeTrans=function(time, now){
+    var dateTimeStamp = Date.parse(time.slice(0,time.indexOf(".")).replace("T"," ").replace(/-/g, "/"));
+    // alert(time);
+    // alert(time.slice(0,time.indexOf(".")).replace("T"," ").replace(/-/g, "/"));
+    // console.log(dateTimeStamp);
+    // alert(now);
+    // alert(dateTimeStamp);
+	var minute = 1000 * 60;
+	var hour = minute * 60;
+	var day = hour * 24;
+	var halfamonth = day * 15;
+	var month = day * 30;
+	var diffValue = now - dateTimeStamp;
+	if(diffValue < 0){return;}
+	var monthC =diffValue/month;
+	var weekC =diffValue/(7*day);
+	var dayC =diffValue/day;
+	var hourC =diffValue/hour;
+	var minC =diffValue/minute;
+	if(monthC>=1){
+		result="" + parseInt(monthC) + "月前";
+	}
+	else if(weekC>=1){
+		result="" + parseInt(weekC) + "周前";
+	}
+	else if(dayC>=1){
+		result=""+ parseInt(dayC) +"天前";
+	}
+	else if(hourC>=1){
+		result=""+ parseInt(hourC) +"小时前";
+	}
+	else if(minC>=1){
+		result=""+ parseInt(minC) +"分钟前";
+	}else{
+        result="刚刚";
+    }
+	return result;
+}
+
 
 function getQueryVariable(variable)
 {
@@ -157,7 +195,6 @@ getDetailInfo = function(){
 getSupport=function(){
     var thistoken = localStorage.getItem("token");
     var numPerPage = 9;
-    
     var page = localStorage.getItem("supportPage");
     $.ajax({
         type:"GET",
@@ -176,21 +213,140 @@ getSupport=function(){
 }
 
 renderSupport=function(data){
+    var now = new Date().getTime();
     console.log("render support")
+    var myid = data.uid;
     var trueData = data.data;
     var content = "";
+    var commentNum;
     for(var i = 0; i < trueData.length; i++){
         content += "<div class=\"panel panel-default\">";
         content += "    <div class=\"panel-heading\">";
         content += trueData[i].name;
         content += "    </div>";
-        content += "        <div class=\"panel-body\">";
+        content += "        <div class=\"panel-body\" onclick=\"showComment("+trueData[i].id+")\">";
         content += trueData[i].content;
+        content +="<br><br><span style=\"color: rgb(200,200,200)\">回帖于"+timeTrans(trueData[i].createTime, now)+"</span>";
         content += "        </div>";
+
+        content += "<div class=\"panel-footer\">";
+        content +="     <div class=\"row\">";
+
+        content +="         <div class=\"col-xs-3  col-sm-3 col-md-3 col-lg-3\">";
+        content +="             <a>";
+        if(trueData[i].count === undefined || trueData[i].count === null){
+            commentNum = "0";
+        }
+        else{
+            commentNum = trueData[i].count;
+        }
+        content +="                 <span class=\"fa fa-fw fa-comment-o\" style=\"color: rgb(246, 141, 0);\">"+ eval(commentNum) +"</span>";
+        content += "            </a>";
+        content +="         </div>";    
+    
+        if(eval(myid) === eval(trueData[i].uid)){
+            content +="         <div class=\"col-xs-1 col-xs-offset-8 col-sm-1 col-sm-offset-8 col-md-1 col-md-offset-8 col-lg-1 col-lg-offset-8\">";
+            content +="             <a href=\"javascript:deleteSupport("+trueData[i].id+");\">";
+            content +="                 <span class=\"fa fa-fw fa-trash-o\" style=\"color: rgb(246, 141, 0);\"></span>";
+            content += "            </a>";
+            content +="         </div>";    
+        }
+		content += "    </div>";
+        content += "</div>";
+    
+
+        content += "    <div id=\"collapse"+trueData[i].id+"\" class=\"panel-collapse collapse\">";
+		// content += "	    <div class=\"panel-body\">";
+		// content += "		    hahha";
+		// content += "	    </div>";
+		content += "    </div>";
         content += "</div>";
     }
     console.log(content);
     $("#Support").html(content);
+}
+
+rerenderComment = function(id){
+    var thistoken = localStorage.getItem("token");
+    var numPerPage = 5;
+    
+    $.ajax({
+        type:"GET",
+        url:url+"/Comment/"+id+"/1/"+numPerPage,
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            renderComment(data, id);
+        }
+    });
+}
+
+showComment = function(id){
+    // console.log($('#collapse'+id).html());
+    var thistoken = localStorage.getItem("token");
+    var numPerPage = 5;
+    
+    $.ajax({
+        type:"GET",
+        url:url+"/Comment/"+id+"/1/"+numPerPage,
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            renderComment(data, id);
+        }
+    });
+    $('#collapse'+id).collapse('toggle');
+}
+
+renderComment = function(Data, id){
+    // var html = "	    <div class=\"panel-body\">";
+    // html += "<ul class=\"list-group\">";
+    var html ="";
+    var data = Data.data;
+    for(var i = 0; i < data.length; i++){
+        html +="<li class=\"list-group-item\" style=\"background-color:#E6E6E6\">";
+        html +="<strong>"+data[i].name+"</strong><br>"+data[i].content;
+        html +="</li>";
+    }
+    if(data.length === 0){
+        html +="<li class=\"list-group-item\" style=\"background-color:#E6E6E6\">";
+        html += "暂无评论";
+        html +="</li>";
+    }
+    if(Data.Finish !== true){
+        html +="<li class=\"list-group-item\" style=\"background-color:#E6E6E6;text-align:center\">";
+        html +="<a href=\"javascript:showAllComment("+id+");\" >显示全部</a> "
+        html +="</li>";
+    }
+    html +="<li class=\"list-group-item\" style=\"background-color:#E6E6E6\">";
+    html +="    <textarea autofocus cols=\"10\" id=\"commentContent"+id+"\" class=\"form-control\" id=\"deblock_udid\" name=\"deblock_udid\" rows=\"2\" placeholder=\"评论几句...\"></textarea>";
+    html +="    <br><button class =\"btn btn-primary\" onclick=\"addCommentNormal("+id+")\">评论</button>"
+    html +="</li>";
+    // html +="</ul>"
+    // html += "	    </div>";
+    $('#collapse'+id).html(html);
+}
+
+renderAllComment = function(Data){
+    var html ="";
+    var data = Data.data;
+    for(var i = 0; i < data.length; i++){
+        html +="<li class=\"list-group-item\">";
+        html +=data[i].name+"<br>"+data[i].content;
+        html +="</li>";
+    }
+    // html +="</ul>"
+    // html += "	    </div>";
+    $("#allCommentBody").html(html);
+
 }
 
 setBasic=function(){
@@ -222,6 +378,7 @@ addSupport=function(){
     $.ajax({
         type:"POST",
         url:url+"/Support/Add",
+        contentType:"application/json",  //发送信息至服务器时内容编码类型。             
         dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
         headers:{
             'token':thistoken
@@ -229,7 +386,12 @@ addSupport=function(){
         data:JSON.stringify(postData),
         success:function(data, status){ 
             console.log("data= ",data, "status=", status);
+            $("#supportCancel").click();
+            $("#supportContent").val("");
             localStorage.setItem("token", thistoken);
+            localStorage.setItem("supportPage","1");
+            getSupport();
+            $("#supportToggle").click();
         }
     });
 }
@@ -330,16 +492,6 @@ collectPost = function(postId){
         });
     }
 
-
-
-
-    // if($("#collects"+postId+" a span").attr("class") === "fa fa-fw fa-bookmark-o"){
-
-    //     $("#collects"+postId+" a span").attr("class","fa fa-fw fa-bookmark");
-    // }
-    // else {
-    //     $("#collects"+postId+" a span").attr("class","fa fa-fw fa-bookmark-o");
-    // }
 }
 
 editPost = function(postId){
@@ -352,4 +504,125 @@ getBasicNotifyFromStorage =function(){
     console.log(Unread);
     $("#Unread").html(Unread);
     $("#notification").html(html);
+}
+
+deletePost = function(id){
+    var isOk = confirm("确定要删除这个帖子吗?");
+    if(isOk){
+        var thistoken = localStorage.getItem("token");
+        $.ajax({
+            type:"PUT",
+            url:url+"/Post/Delete/"+postId,
+            dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+            headers:{
+                'token':thistoken
+            },
+            success:function(data, status){ 
+                console.log("data= ",data, "status=", status);
+                localStorage.setItem("token", thistoken);
+                alert("删除成功");
+                self.location.href="home.html";
+            }
+        });    
+    }
+    else {
+        ;
+    }
+}
+
+showAllComment=function(id){
+    getAllComment(id);
+    localStorage.setItem("supportIdNow",id);
+    $("#allComment").modal("show");
+}
+
+addCommentModal=function(){
+    var supportIdNow = localStorage.getItem("supportIdNow");
+    var commentContent = $("#commentContent").val();
+    // alert(commentContent + supportIdNow);
+    var CommentData={
+        "content": commentContent,
+        "supportId": supportIdNow
+    }
+
+    var thistoken = localStorage.getItem("token");
+    $.ajax({
+        type:"POST",
+        url:url+"/Comment/Add",
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        contentType:"application/json",  //发送信息至服务器时内容编码类型。             
+        data:JSON.stringify(CommentData),
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            alert("评论成功");
+            $("#allComment").modal("hide");
+            $("#commentContent").val("");
+            rerenderComment(supportIdNow);
+            // getSupport();
+        }
+    });
+
+}
+
+
+
+addCommentNormal = function(id){
+    var commentContent = $("#commentContent"+id).val();
+    var CommentData={
+        "content": commentContent,
+        "supportId": id
+    }
+
+    var thistoken = localStorage.getItem("token");
+    $.ajax({
+        type:"POST",
+        url:url+"/Comment/Add",
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        contentType:"application/json",  //发送信息至服务器时内容编码类型。             
+        data:JSON.stringify(CommentData),
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            alert("评论成功");
+            rerenderComment(id);
+            // getSupport();
+        }
+    });
+    // alert(commentContent);
+}
+
+deleteSupport = function(id){
+    var putData={
+        "id":id
+    }
+    var isOk = confirm("确定要删除您的回帖吗?");
+    if(isOk){
+        var thistoken = localStorage.getItem("token");
+        $.ajax({
+            type:"PUT",
+            url:url+"/Support/Delete/",
+            dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+            contentType:"application/json",  //发送信息至服务器时内容编码类型。             
+            data:JSON.stringify(putData),
+            headers:{
+                'token':thistoken
+            },
+            success:function(data, status){ 
+                console.log("data= ",data, "status=", status);
+                localStorage.setItem("token", thistoken);
+                alert("删除成功");
+                getSupport();
+            }
+        });
+    }
+    else {
+        ;
+    }
 }
--- js/mine.js
+++ js/mine.js
@@ -12,11 +12,14 @@ $(function()
     }
     else{
         localStorage.setItem("timelinePage", "1");
+        localStorage.setItem("postPage", "1");
+        localStorage.setItem("collectionPage", "1");
         url = localStorage.getItem("url");
         // alert(url);
         getOperation();
-        getMyPost();
-        getMyCollection();
+        getPost();
+        getCollection();
+        getUserInfo();
         getBasicNotifyFromStorage();
         setBasic();
     }
@@ -28,6 +31,7 @@ setBasic=function(){
     console.log(username);
     console.log(user);
     $("li#userinfo").html(user);
+
 }
 
 
@@ -59,12 +63,61 @@ getOperation = function(){
     });
 }
 
-getMyPost=function(){
-    ;
+getPost=function(){
+    var thistoken = localStorage.getItem("token");
+    var postPage = localStorage.getItem("postPage");
+    var numPerPage = 9;
+    $.ajax({
+        type:"GET",
+        url:url+"/Post/UserPost/"+postPage+"/"+numPerPage,
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            renderPost(data);
+        }
+    });
 }
 
-getMyCollection = function(){
-    ;
+getCollection = function(){
+    var thistoken = localStorage.getItem("token");
+    var collectionPage = localStorage.getItem("collectionPage");
+    var numPerPage = 9;
+    $.ajax({
+        type:"GET",
+        url:url+"/Post/Collection/"+collectionPage+"/"+numPerPage,
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            renderCollection(data);
+        }
+    });
+}
+
+getUserInfo = function(){
+    var thistoken = localStorage.getItem("token");
+    var collectionPage = localStorage.getItem("collectionPage");
+    var numPerPage = 9;
+    $.ajax({
+        type:"GET",
+        url:url+"/User/Info",
+        dataType:"json",  // 预期服务器返回的数据类型。如果不指定，jQuery 将自动根据 HTTP 包 MIME 信息来智能判断，比如XML MIME类型就被识别为XML。
+        headers:{
+            'token':thistoken
+        },
+        success:function(data, status){ 
+            console.log("data= ",data, "status=", status);
+            localStorage.setItem("token", thistoken);
+            renderUserInfo(data);
+        }
+    });
 }
 
 resizeTitle = function(title){
@@ -79,7 +132,8 @@ resizeTitle = function(title){
 }
 
 timeTrans=function(time, now){
-    var dateTimeStamp = Date.parse(time.slice(0,time.indexOf(".")).replace("T"," "));
+    var dateTimeStamp = Date.parse(time.slice(0,time.indexOf(".")).replace("T"," ").replace(/-/g, "/"));
+    // var dateTimeStamp = Date.parse(time.slice(0,time.indexOf(".")).replace("T"," "));
     // console.log(dateTimeStamp);
 	var minute = 1000 * 60;
 	var hour = minute * 60;
@@ -119,7 +173,7 @@ renderOperation=function(Data){
     var html = "";
     var data = Data.data;
     for(var i = 0; i < data.length; i++){
-        html +="<div class=\"timeline-card\">";
+        html +="<div class=\"timeline-card\" onclick=\"gotoDetail(\""+data[i].id+"\")\">";
         html +="    <div class=\"timeline-header\">";
         html +="        <img class=\"timeline-avatar\" src=\"./testIcon.jpg\"/>";
         html +="    <div class=\"timeline-username\"></div>";
@@ -133,12 +187,12 @@ renderOperation=function(Data){
         html +="        <div class=\"row\">";
         html +="            <div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2\">";
         html +="                <a>";
-        html +="                    <span id=\"collects\" class=\"fa fa-fw fa-bookmark\" style=\"color: rgb(60, 79, 213);\">5</span>";
+        html +="                    <span id=\"collects\" class=\"fa fa-fw fa-bookmark\" style=\"color: rgb(60, 79, 213);\">"+data[i].collectionNum+"</span>";
         html +="                </a>";
         html +="            </div>";
         html +="            <div class=\"col-xs-2 col-xs-offset-3 col-sm-2 col-sm-offset-3 col-md-2 col-md-offset-3 col-lg-2 col-lg-offset-3\">"
         html +="                <a>";
-        html +="                    <span id=\"collects\" class=\"fa fa-fw fa-heart\" style=\"color: rgb(60, 79, 213);\">5</span>";
+        html +="                    <span id=\"likes\" class=\"fa fa-fw fa-heart\" style=\"color: rgb(255, 60, 60);\">"+data[i].likeNum+"</span>";
         html +="                </a>";
         html +="            </div>";
         html +="        </div>";
@@ -146,9 +200,94 @@ renderOperation=function(Data){
         html +="</div>";
         html +="</div>";
     }
-    $("#my-timeline").html(html);
+    if(data.length !== 0){
+        $("#my-timeline").html(html);
+    }
 }
 
-renderMyPost=function(){
-    ;
+renderPost=function(Data){
+    var now = new Date().getTime();
+    // console.log(now);
+    var html = "";
+    var data = Data.data;
+    for(var i = 0; i < data.length; i++){
+        html +="<div class=\"timeline-card\" onclick=\"gotoDetail(\""+data[i].id+"\")\">";
+        html +="    <div class=\"timeline-header\">";
+        html +="        <img class=\"timeline-avatar\" src=\"./testIcon.jpg\"/>";
+        html +="    <div class=\"timeline-username\">"+data[i].name+"</div>";
+        html +="    <div>"+timeTrans(data[i].createTime, now)+"</div>";
+        html +="    <div style=\"margin-left: 5px\">发布了</div>";
+        html +="</div>";
+        html +="<div class=\"timeline-content\">";
+        html +="    <div class=\"post-title\">"+resizeTitle(data[i].title)+"</div>";
+        html +="    <div class=\"post-content\">"+data[i].summary+"</div>";
+        html +="    <div class=\"content-item\">";
+        html +="        <div class=\"row\">";
+        html +="            <div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2\">";
+        html +="                <a>";
+        html +="                    <span id=\"collects\" class=\"fa fa-fw fa-bookmark\" style=\"color: rgb(60, 79, 213);\">"+data[i].collectionNum+"</span>";
+        html +="                </a>";
+        html +="            </div>";
+        html +="            <div class=\"col-xs-2 col-xs-offset-3 col-sm-2 col-sm-offset-3 col-md-2 col-md-offset-3 col-lg-2 col-lg-offset-3\">"
+        html +="                <a>";
+        html +="                    <span id=\"likes\" class=\"fa fa-fw fa-heart\"style=\" color: rgb(255, 60, 60);\">"+data[i].likeNum+"</span>";
+        html +="                </a>";
+        html +="            </div>";
+        html +="        </div>";
+        html +="    </div>";
+        html +="</div>";
+        html +="</div>";
+    }
+    if(data.length !== 0){
+        $("#my-publish").html(html);
+    }
+}
+
+renderCollection=function(Data){
+    var now = new Date().getTime();
+    // console.log(now);
+    var html = "";
+    var data = Data.data;
+    for(var i = 0; i < data.length; i++){
+        html +="<div class=\"timeline-card\" onclick=\"gotoDetail(\""+data[i].id+"\")\">";
+        html +="    <div class=\"timeline-header\">";
+        html +="        <img class=\"timeline-avatar\" src=\"./testIcon.jpg\"/>";
+        html +="    <div class=\"timeline-username\"></div>";
+        html +="    <div>"+timeTrans(data[i].createTime, now)+"</div>";
+        html +="    <div style=\"margin-left: 5px\">收藏了</div>";
+        html +="</div>";
+        html +="<div class=\"timeline-content\">";
+        html +="    <div class=\"post-title\">"+resizeTitle(data[i].title)+"</div>";
+        html +="    <div class=\"post-content\">"+data[i].summary+"</div>";
+        html +="    <div class=\"content-item\">";
+        html +="        <div class=\"row\">";
+        html +="            <div class=\"col-xs-2 col-sm-2 col-md-2 col-lg-2\">";
+        html +="                <a>";
+        html +="                    <span id=\"collects\" class=\"fa fa-fw fa-bookmark\" style=\"color: rgb(60, 79, 213);\">"+data[i].collectionNum+"</span>";
+        html +="                </a>";
+        html +="            </div>";
+        html +="            <div class=\"col-xs-2 col-xs-offset-3 col-sm-2 col-sm-offset-3 col-md-2 col-md-offset-3 col-lg-2 col-lg-offset-3\">"
+        html +="                <a>";
+        html +="                    <span id=\"likes\" class=\"fa fa-fw fa-heart\" style=\"color: rgb(255, 60, 60);\">"+data[i].likeNum+"</span>";
+        html +="                </a>";
+        html +="            </div>";
+        html +="        </div>";
+        html +="    </div>";
+        html +="</div>";
+        html +="</div>";
+    }
+    if(data.length !== 0){
+        $("#my-star").html(html);
+    }
+}
+
+renderUserInfo=function(data){
+    var html = "";
+    html += "<h4 class=\"media-heading\">"+data.name+"</h4>"
+    html += "<p>"+data.description+"</p>";
+    $("#UserInfo").html(html);
+}
+
+gotoDetail=function(id){
+    self.location.href="PostDetail.html?postid="+id;
 }
